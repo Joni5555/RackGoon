7e5047e6ad74fe3141d04151c26c41d2c657eb64
KeNsHiYaYo <ezzeyayo@gmail.com>
2024-10-19 11:02:46 -0600

    Zombie

--- Assets/Assets/enemies/Zombie.cs
+++ Assets/Assets/enemies/Zombie.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-    public int MoveSpeed;
-    public float MaxDisJugador;
-    public float MinDistJugador;
-    public Transform Player;
+    public int MoveSpeed; //Movimiento del enemigo
+    public float MaxDisJugador; //La distancia Maxima en la que localiza al enemigo
+    public float MinDistJugador; //Minima distancia en la que localiza al anemigo
+    public Transform Player; 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Vector2 EnemyPos = transform.position;
+        Vector2 PlayerPos = Player.position;
+        float distancia =
+            Vector2.Distance(EnemyPos, PlayerPos);
+
+        if (distancia >= MinDistJugador && distancia <= MaxDisJugador)
+        {
+            Vector2 targetPos = new Vector2(Player.position.x, this.transform.position.y);
+            transform.LookAt(targetPos);
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 EnemyPos = transform.position;
-        Vector3 PlayerPos = Player.position;
-        float distancia =
-            Vector3.Distance(EnemyPos, PlayerPos);
-
-        if (distancia >= MinDistJugador && distancia <= MaxDisJugador)
-        {
-            Vector3 targetPos = new Vector3(Player.position.x, this.transform.position.y,Player.position.z);
-            transform.LookAt(targetPos);
-            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-        }
     }
 }
